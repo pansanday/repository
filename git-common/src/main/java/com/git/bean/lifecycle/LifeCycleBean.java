5192322dc2e81fc7b00c5f224b37296dba170714
Pansanday <pansanday@gmail.com>
2014-06-21 08:53:43 +0800

    add some comment to LifeCycleBean.java

--- git-common/src/main/java/com/git/bean/lifecycle/LifeCycleBean.java
+++ git-common/src/main/java/com/git/bean/lifecycle/LifeCycleBean.java
@@ -31,6 +31,10 @@ public class LifeCycleBean implements InitializingBean {
         System.out.println("Implements initializing end!");
     }
 
+	/**
+	 * bean在配置文件中被实例化之后,会调用这个方法进行初始化,因为配置了init-method="inits";
+	 * 这个inits名称可以随意指定,只要保证和配置文件中名称相同就行
+	 */
     public void inits() {
         System.out.println("init() initializing start...");
         if (null == name) {
@@ -39,6 +43,10 @@ public class LifeCycleBean implements InitializingBean {
         System.out.println("init() initializing end!");
     }
 
+    /**
+     * applicaton context在被关闭后,会在bean工厂中销毁所有的bean,此时调用该方法
+     * 同理也是因为配置了destroy-method="destroy",destroy这个名称可变
+     */
     public void destroy() {
         System.out.println("destory()....");
     }
