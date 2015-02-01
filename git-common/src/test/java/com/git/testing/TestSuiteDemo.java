4b4ba1b49c7db3999a7192a437ea915dc93776dd
pansanday <pansanday@gmail.com>
2015-02-01 16:18:18 +0800

    1.更新了测试用例,增加了几个test case 2.对已有测试用例进行了调整和注释

--- git-common/src/test/java/com/git/testing/TestSuiteDemo.java
+++ git-common/src/test/java/com/git/testing/TestSuiteDemo.java
@@ -3,7 +3,8 @@
  */
 package com.git.testing;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import junit.framework.Test;
@@ -17,6 +18,7 @@ import junit.framework.TestSuite;
 public class TestSuiteDemo extends TestCase {
 
     FileReader _input = null;
+    FileReader _empty = null;
     
     /**
      * 构造函数
@@ -31,9 +33,17 @@ public class TestSuiteDemo extends TestCase {
     protected void setUp() {
         try {
             _input = new FileReader("./src/main/resources/data.txt");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("unable to open test file");
+            _empty = newEmptyFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e.toString());
         }
+    }
+    
+    private FileReader newEmptyFile() throws IOException {
+        File emptyFile = new File("empty.txt");
+        FileOutputStream out = new FileOutputStream(emptyFile);
+        out.close();
+        return new FileReader(emptyFile);
     }
 
     /**
@@ -59,20 +69,80 @@ public class TestSuiteDemo extends TestCase {
             // assertEquals('w', ch);
         }
     }
+    
+    /**
+     * 测试读到文件末尾时的返回值
+     */
+    public void testReadAtEnd() throws IOException {
+        int ch = -1234;
+        for (int i = 0; i < 5; i++) {
+            ch = _input.read();
+        }
+        // 当输入流到达文件尾端,read()应该返回-1
+        assertEquals(-1, _input.read());
+    }
+    
+    /**
+     * 测试边界条件
+     */
+    public void testReadBoundaries() throws IOException {
+        assertEquals("read first char", '1', _input.read());
+        int ch;
+        for (int i = 1; i < 4; i++) {
+            ch = _input.read();
+        }
+        // 测试读取最后一个字符
+        assertEquals("read last char", '5', _input.read());
+        assertEquals("read at end", -1, _input.read());
+        // 测试读取文件末尾之后的位置,同样也应该返回-1
+        assertEquals("readpast end", -1, _input.read());
+    }
+    
+    /**
+     * 测试读取空文件
+     */
+    public void testEmptyRead() throws IOException {
+        assertEquals(-1, _empty.read());
+    }
+    
+    /**
+     * 检查预期的错误是否如期出现
+     * 如果尝试在关闭流后再读取它,应该会得到一个IOException异常
+     */
+    public void testReadAfterClose() throws IOException {
+        _input.close();
+        try {
+            _input.read();
+            fail("no exception for read past end");
+        } catch (IOException io) {}
+    }
 
     /**
+     * Run As -> JUnit Test
      * Method used to: 将测试代码集中到测试套件中
      * @return
      */
     public static Test suite() {
         TestSuite suite = new TestSuite();
-        // 将待测函数的名称以字符串形式传递给构造函数
-        suite.addTest(new TestSuiteDemo("testRead"));
+        // 方法一: 将待测函数的名称以字符串形式传递给构造函数
+        // suite.addTest(new TestSuiteDemo("testRead"));
+        // suite.addTest(new TestSuiteDemo("testReadAtEnd"));
+
+        // 方法二: 可以向下面这样,在测试套件中包含其他测试套件
+        suite.addTest(new TestSuite(TestSuiteDemo.class));
         return suite;
     }
 
+    /**
+     * Run As -> Java Application
+     * 使用main方法,以字符的方式运行测试用例
+     */
     public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+        // 运行测试套件
+        // junit.textui.TestRunner.run(suite());
+        
+        // 传入一个类,这样测试框架会自动寻找那些以"test"开头的测试用例,并将其添加到TestSuite中
+        junit.textui.TestRunner.run(new TestSuite(TestSuiteDemo.class));
     }
 
 }
