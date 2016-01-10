3a4bf1c6179858ae899788563f9347489d287434
pansanday <pansanday@pansanday-PC>
2016-01-10 23:27:46 +0800

    增加oracle jdbc的例子

---
+++ git-common/src/main/java/com/git/jdbc/ConnectionFactory.java
@@ -0,0 +1,61 @@
+package com.git.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author pansanday
+ * @since 2016年1月10日
+ */
+public class ConnectionFactory {
+
+    final static Logger logger = LoggerFactory.getLogger(ConnectionFactory.class);
+
+    private static ResourceBundle bundle = ResourceBundle.getBundle("jdbc");
+    private static String driverClass = bundle.getString("driverClass");
+    private static String jdbcUrl = bundle.getString("jdbcUrl");
+    private static String userName = bundle.getString("user");
+    private static String password = bundle.getString("password");
+
+    private static Connection connection = null;
+
+    /**
+     * 获取连接
+     * @return
+     */
+    public static Connection getConnection() {
+        try {
+            Class.forName(driverClass);
+            connection = DriverManager.getConnection(jdbcUrl, userName, password);
+            return connection;
+        } catch (ClassNotFoundException e) {
+            logger.error("no definition for the class with the specified name could be found");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            logger.error("database access error or other errors");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * 释放连接
+     * TODO:没有效果,不知道为啥
+     */
+    public static void releaseConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                logger.error("释放connection时出错!");
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ git-common/src/test/java/com/git/jdbc/ConnectionFactoryTest.java
@@ -0,0 +1,44 @@
+package com.git.jdbc;
+
+import static org.junit.Assert.assertEquals;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.junit.Test;
+
+/**
+ * @author pansanday
+ * @since 2016年1月10日
+ */
+public class ConnectionFactoryTest {
+
+    @Test
+    public void testGetConnection() {
+        Connection connection = null;
+        try {
+            connection = ConnectionFactory.getConnection();
+            PreparedStatement pStatement = connection.prepareStatement("select * from dual");
+            ResultSet rs = pStatement.executeQuery();
+            String result = "";
+            while (rs.next()) {
+                result = rs.getString("DUMMY");
+            }
+            assertEquals("两者不相等", "X", result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                    connection = null;
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            /*
+             * if (connection == null) { System.out.println("ok"); }
+             */
+        }
+    }
+}
