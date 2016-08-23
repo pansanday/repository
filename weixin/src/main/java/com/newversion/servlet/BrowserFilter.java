6cd22d0b41d600b83a29fd52c616c22895428130
pansanday <pansanday@gmail.com>
2016-08-23 17:30:57 +0800

    解决乱码问题

--- weixin/src/main/java/com/newversion/servlet/BrowserFilter.java
+++ weixin/src/main/java/com/newversion/servlet/BrowserFilter.java
@@ -23,17 +23,16 @@ public class BrowserFilter implements Filter {
 	/**
 	 * 过滤器的核心是doFilter()方法,对于客户端请求进行过滤.首先获取到客户端浏览器的User Agent,然后判断是不是微信浏览器,如果是就继续往下走(允许访问被保护的资源),如果不是将会在页面显示提示"请使用微信浏览器访问!"
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		String userAgent = req.getHeader("User-Agent");
 		// 微信内置浏览器
-		if (userAgent.contains("MicroMessager")) {
+		if (userAgent.contains("MicroMessenger")) {
 			chain.doFilter(request, response);
 		} else { 
 			// 其他浏览器
 			HttpServletResponse resp = (HttpServletResponse) response;
-			resp.setCharacterEncoding("UTF-8");
+			resp.setCharacterEncoding("gb2312");
 			// 输出提示
 			PrintWriter out = resp.getWriter();
 			out.write("请使用微信浏览器访问!");
